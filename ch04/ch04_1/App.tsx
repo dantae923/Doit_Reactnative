9bf644a1b027059e732c5d5dce3bbdaee1f70334
dantae923 <youngjoonkim94@gmail.com>
2021-10-01 16:09:26 +0900

    init

---
+++ ch04/ch04_1/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {StyleSheet, SafeAreaView, Text} from 'react-native'
+import {useClock} from './src/hooks'
+
+export default function App(){
+  const time = useClock()
+  
+  return (
+    <SafeAreaView style={styles.safeAreaView}>
+      <Text style={[styles.digitFont, styles.time]}>
+        {time.toLocaleTimeString()}
+      </Text>
+      <Text style={styles.digitFont}>{time.toLocaleDateString()}</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  digitFont: {fontFamily: 'MajorMonoDisplay-Regular', fontWeight: '400'},
+  time: {fontSize: 50}
+})
