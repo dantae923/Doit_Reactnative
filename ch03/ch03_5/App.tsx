8ec87403b5890535f7f752be2dc32dcf42b6a3b6
dantae923 <youngjoonkim94@gmail.com>
2021-10-01 16:09:05 +0900

    init

---
+++ ch03/ch03_5/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {SafeAreaView, StyleSheet, FlatList, View} from 'react-native'
+import {Colors} from 'react-native-paper'
+import color from 'color'
+import Person, { PersonProps } from './src/copy/Person'
+import * as D from './src/data'
+
+const people: D.IPerson[] = D.makeArray(10).map(D.createRandomPerson)
+// prettier-ignore
+export default function App(){
+  return (
+    <SafeAreaView style={styles.flex}>
+      <FlatList data={people} renderItem={({item}) => <Person person={item}></Person>}
+                keyExtractor={(item, index) => item.id} ItemSeparatorComponent={() => <View style={styles.itemSeparator}></View>}>
+      </FlatList>
+    </SafeAreaView>
+  )
+}
+// prettier-ignore
+const styles = StyleSheet.create({
+  flex: {flex: 1},
+  itemSeparator: {borderWidth: 1, borderColor: color(Colors.grey500).lighten(0.3).string()}
+})
