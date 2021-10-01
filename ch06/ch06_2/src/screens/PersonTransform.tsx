5a58ee721cb6f11df9f39d243ed404d908b07174
dantae923 <youngjoonkim94@gmail.com>
2021-10-01 16:11:25 +0900

    init

---
+++ ch06/ch06_2/src/screens/PersonTransform.tsx
@@ -0,0 +1,85 @@
+import React, {useCallback, useState} from "react";
+import type {FC} from 'react'
+import {View, Text, Image, Animated, Easing} from 'react-native'
+import {Colors} from 'react-native-paper'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import * as D from '../data'
+import {useToggle, useAnimatedValue, useStyle} from '../hooks'
+import {styles} from './Person.style'
+import moment from "moment-with-locales-es6";
+import {Avatar} from '../components'
+import { interpolate } from "../utils";
+
+export type PersonProps = {
+    person: D.IPerson
+    deletePressed: () => void
+}
+
+const PersonTransform: FC<PersonProps> = ({person: initialPerson, deletePressed}) => {
+    const [person, setPerson] = useState<D.IPerson>(initialPerson)
+    const [started, toggleStarted] = useToggle(false)
+    const animValue = useAnimatedValue(0)
+    
+    const nameAnimStyle = useStyle({
+        transform: [
+            {
+                translateX: interpolate(animValue, [0, 500])
+            }
+        ]
+    })
+    const emailAnimStyle = useStyle({
+        marginTop: started ? 20 : 0,
+        transform: [
+            {
+                rotate: interpolate(animValue, ['0deg', '180deg'])
+            }
+        ]
+    })
+    const commentAnimStyle = useStyle({
+        transform: [
+            {
+                translateY: interpolate(animValue, [0, 200])
+            },
+            {
+                rotate: interpolate(animValue, ['0deg', '45deg'])
+            },
+            {
+                scale: interpolate(animValue, [1, 2])
+            }
+        ]
+    })
+
+    const avatarPressed = useCallback(() => Animated.timing(animValue, {
+        useNativeDriver: false,
+        toValue: started ? 0 : 1,
+        easing: Easing.bounce
+    }).start(toggleStarted), [started])
+
+    return (
+        <View style={[styles.view]}>
+            <View style={[styles.leftView]}>
+                <Avatar imageStyle={[styles.avatar]} uri={person.avatar} size={50} onPress={avatarPressed}></Avatar>
+                <Text style={[styles.text]}>Press Me</Text>
+            </View>
+            <View style={[styles.rightView]}>
+                <Animated.Text style={[styles.name, nameAnimStyle]}>{person.name}</Animated.Text>
+                <Animated.Text style={[styles.email, emailAnimStyle]}>{person.email}</Animated.Text>
+                <View style={[styles.dateView]}>
+                    <Text style={[styles.text]}>
+                        {moment(person.createdDate).startOf('day').fromNow()}
+                    </Text>
+                    <Icon name='trash-can-outline' size={26} color={Colors.lightBlue500} onPress={deletePressed}></Icon>
+                </View>
+                <Animated.Text numberOfLines={3} ellipsizeMode="tail" style={[styles.comments, commentAnimStyle]}>{person.comments}</Animated.Text>
+                <Image style={[styles.image]} source={{uri: person.image}}></Image>
+                <View style={[styles.countsView]}>
+                    <Icon name="comment" size={24} color={Colors.blue500}></Icon>
+                    <Icon name="twitter-retweet" size={24} color={Colors.purple500}></Icon>
+                    <Icon name="heart" size={24} color={Colors.red500}></Icon>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default PersonTransform
---
+++ ch06/ch06_2/src/screens/Transform.tsx
@@ -0,0 +1,42 @@
+import React, {useState, useCallback, useEffect} from "react";
+import {StyleSheet, View, Text, Switch, FlatList} from 'react-native'
+import {useTheme} from 'react-native-paper'
+import {useToggleTheme} from '../contexts'
+import * as D from '../data'
+import Person from "./PersonTransform";
+
+export default function People(){
+    const [people, setPeople] = useState<D.IPerson[]>([])
+    const theme = useTheme()
+    const toggleTheme = useToggleTheme()
+    const addPerson = useCallback(() => {setPeople((people) => [D.createRandomPerson(), ...people])}, [])
+    const removeAllPersons = useCallback(() => {setPeople((notUsed) => [])}, [])
+    const deletePerson = useCallback((id: string) => () => setPeople((people) => people.filter((person) => person.id != id)), [])
+
+    useEffect(addPerson, [])
+
+    return (
+        <View style={[styles.view, {backgroundColor: theme.colors.surface}]}>
+            <View style={[styles.topBar, {backgroundColor: theme.colors.accent}]}>
+                <Text onPress={addPerson} style={styles.text}>
+                    add
+                </Text>
+                <Text onPress={removeAllPersons} style={styles.text}>
+                    remove all
+                </Text>
+                <View style={{flex: 1}}></View>
+                <Switch value={theme.dark} onValueChange={toggleTheme}></Switch>
+            </View>
+            <FlatList
+                data={people} 
+                renderItem={({item}) => <Person person={item} deletePressed={deletePerson(item.id)}></Person>}
+                keyExtractor={(item) => item.id}></FlatList>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    view: {flex: 1},
+    topBar: {flexDirection: 'row', padding: 5},
+    text: {marginRight: 10, fontSize: 20}
+})
